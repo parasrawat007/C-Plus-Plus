7a2f8bd8e3e9b6b0a062b848c156affe9464e1dc
Paras Rawat <parasrawat67@gmail.com>
2020-12-05 14:35:20 +0530

    Variable Initialization

---
+++ 3_Initialization.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+    int a=10;  //Simple Initialization
+    int b(20); //Constructor Initialization
+    int c{30}; //Uniform Initialization
+
+    return 0;
+}
